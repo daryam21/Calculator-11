ff7b36f57cd64d836be34d403cc509cc04b77c64
idati <specht.hartweger@hotmail.de>
2016-02-01 05:55:30 +0100

    Add Weather API

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,5 +30,81 @@ namespace Calculator
             Application.Current.DebugSettings.EnableFrameRateCounter = false;
 
         }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city.Text + "," + country.Text + "&appid=44db6a862fba0b067b1930da0d769e98";
+            HttpClient client = new HttpClient();
+            string stringdate = await client.GetStringAsync(new Uri(url));
+            RootObject data = JsonConvert.DeserializeObject<RootObject>(stringdate);
+
+            result1.Text = data.main.temp_max.ToString();
+            result2.Text = data.main.temp_min.ToString();
+            result3.Text = data.weather[0].main;
+
+
+        }
     }
 }
+
+
+public class Coord
+{
+    public double lon { get; set; }
+    public double lat { get; set; }
+}
+
+public class Weather
+{
+    public int id { get; set; }
+    public string main { get; set; }
+    public string description { get; set; }
+    public string icon { get; set; }
+}
+
+public class Main
+{
+    public double temp { get; set; }
+    public int pressure { get; set; }
+    public int humidity { get; set; }
+    public double temp_min { get; set; }
+    public double temp_max { get; set; }
+}
+
+public class Wind
+{
+    public double speed { get; set; }
+    public int deg { get; set; }
+}
+
+public class Clouds
+{
+    public int all { get; set; }
+}
+
+public class Sys
+{
+    public int type { get; set; }
+    public int id { get; set; }
+    public double message { get; set; }
+    public string country { get; set; }
+    public int sunrise { get; set; }
+    public int sunset { get; set; }
+}
+
+public class RootObject
+{
+    public Coord coord { get; set; }
+    public List<Weather> weather { get; set; }
+    public string @base { get; set; }
+    public Main main { get; set; }
+    public Wind wind { get; set; }
+    public Clouds clouds { get; set; }
+    public int dt { get; set; }
+    public Sys sys { get; set; }
+    public int id { get; set; }
+    public string name { get; set; }
+    public int cod { get; set; }
+}
+
+
